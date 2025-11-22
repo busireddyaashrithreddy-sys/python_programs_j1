b90f1f9e1d52f647e5f68e72d93e1414767f7e63
busireddyaashrithreddy-sys <busireddyaashrithreddy@gmail.com>
2025-11-22 09:43:39 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#data types-in python 3 types
+#1.int(integer)
+#2.str(string)
+#3.float(decimals)
+name="aashrith123"
+age=16
+salary=10000.52
+data="1234"
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
---
+++ variables.py
@@ -0,0 +1,7 @@
+#variables-is a memory allocation to a value
+name="aashrith" 
+age=16
+salary=10000.00
+print(name)
+print(age)
+print(salary)        
